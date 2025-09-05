b290e72f9498bf0d4955acc27657dd51884beb2b
Kevin Ory <kevinaja2020@gmail.com>
2025-09-05 16:51:37 +0800

    clearing past schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,7 @@
 import { defineSchema } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
-import { sawerTables } from "./_tables/sawerData";
 
 
 export default defineSchema({
-    ...sawerTables,
     ...authTables,
 })
